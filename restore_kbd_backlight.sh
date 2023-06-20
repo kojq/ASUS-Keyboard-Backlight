42eedc46f516f380ae7f0f87a4625764f8835bdc
kojq <106934605+kojq@users.noreply.github.com>
2023-06-20 17:04:26 -0400

    Add files via upload

---
+++ restore_kbd_backlight.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+case $1 in
+    "save")
+        cp /sys/class/leds/asus::kbd_backlight/brightness /etc/keyboard/brightness
+        ;;
+    "restore")
+        cp /etc/keyboard/brightness /sys/class/leds/asus::kbd_backlight/brightness
+        ;;
+esac
