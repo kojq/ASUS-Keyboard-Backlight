30a3211dfd655a5de07b791079f62147e8b0dd94
kojq <106934605+kojq@users.noreply.github.com>
2023-06-20 18:42:30 -0400

    Update restore_kbd_backlight.sh

--- restore_kbd_backlight.sh
+++ restore_kbd_backlight.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 case $1 in
-    "save")
-        cp /sys/class/leds/asus::kbd_backlight/brightness /etc/keyboard/brightness
-        ;;
-    "restore")
-        cp /etc/keyboard/brightness /sys/class/leds/asus::kbd_backlight/brightness
-        ;;
+    save)
+        cp /sys/class/leds/asus::kbd_backlight/brightness /etc/keyboard/brightness ;;
+    restore)
+        cp /etc/keyboard/brightness /sys/class/leds/asus::kbd_backlight/brightness ;;
 esac
